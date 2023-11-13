1c31ff3a1129766d457e347637cf64ef05fb75be
ch3rryl <cherryl@live.co.uk>
2023-11-13 12:30:40 +0000

    update strings & operators exercise

--- exercises/java-primitives/src/main/java/com/cbfacademy/operators/App.java
+++ exercises/java-primitives/src/main/java/com/cbfacademy/operators/App.java
@@ -6,32 +6,34 @@ public class App {
     }
 
     public static double add(double operand1, double operand2) {
-        System.err.println(operand1 + operand2);
-        throw new RuntimeException("Not implemented");
+        return operand1 + operand2;
+  
     }
 
     public static double subtract(double operand1, double operand2) {
-        System.out.println(operand1 - operand2);
-        throw new RuntimeException("Not implemented");
+        return operand1 - operand2;
+   
     }
 
     public static double multiply(double operand1, double operand2) {
-        System.out.println(operand1 * operand2);
-        throw new RuntimeException("Not implemented");
+        return operand1 * operand2;
+   
     }
 
     public static Boolean areEqual(double operand1, double operand2) {
-        System.out.println(operand1 == operand2);
-        throw new RuntimeException("Not implemented");
+        System.out.println(operand1.equals(operand2));
+        // return operand1.equals(operand2);
+   
     }
 
     public static Boolean isLessThan(double operand1, double operand2) {
+        return operand1 > operand2;
         System.out.println(operand1 > operand2);
-        throw new RuntimeException("Not implemented");
+   
     }
 
     public static Boolean isMoreThan(double operand1, double operand2) {
-        System.out.println(operand1 > operand2);
-        throw new RuntimeException("Not implemented");
+        return operand1 > operand2;
+  
     }
 }
--- exercises/java-primitives/src/main/java/com/cbfacademy/strings/App.java
+++ exercises/java-primitives/src/main/java/com/cbfacademy/strings/App.java
@@ -7,19 +7,19 @@ public class App {
 
     public static String concatenate(String word1, String word2, String word3) {
         // TODO: Write code that concatenates the input parameters and returns the result
-        System.out.println(word1 + " " + word2 + " " + word3);
-        throw new RuntimeException("Not implemented");
+        return word1 + " " + word2 + " " + word3;
+        
     }
 
     public static Boolean areEqual(String word1, String word2) {
         // TODO: Write code to determine whether the input parameters are equal strings
-        System.out.println(word1 == word2);
-        throw new RuntimeException("Not implemented");
+        System.out.println(word1.equals(word2));
+
     }
 
     public static String format(String item, int quantity, double price) {
-        // TODO: Write code to return a string formatted as follows: "Item: [item]. Price: £[amount]. Quantity: [quantity]". The date should be formatted as DD/MM/YYYY, e.g. 24/10/2023
-        
-        throw new RuntimeException("Not implemented");
+        // TODO: Write code to return a string formatted as follows: "Item: %s. Price: £%d. Quantity: [quantity]". The date should be formatted as DD/MM/YYYY, e.g. 24/10/2023
+        return String.format("Item: %s. Price: £ %d. Quantity: %d");
+       
     }
 }
