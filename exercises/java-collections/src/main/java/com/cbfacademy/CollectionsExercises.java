bb36ee874610c323112648a7f4e39018856e69f6
ch3rryl <cherryl@live.co.uk>
2023-11-22 15:16:09 +0000

    complete collections exercise. test not complete

--- exercises/java-collections/src/main/java/com/cbfacademy/CollectionsExercises.java
+++ exercises/java-collections/src/main/java/com/cbfacademy/CollectionsExercises.java
@@ -6,10 +6,13 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Stack;
 
+
+
 public class CollectionsExercises {
+    
 
     public LinkedList<Integer> useLinkedList() {
-        LinkedList<Integer> list1= new LinkedList<>();
+        LinkedList<Integer> list1 = new LinkedList<>();
         list1.add(4);
         list1.add(5);
         list1.add(6);
@@ -17,16 +20,17 @@ public class CollectionsExercises {
         list1.add(2);
         list1.add(9);
         list1.add(2);
+
+        // Index then number to add === add the number 4 at position 3 which = index 2 (0,1, 2)
         list1.add(2, 4);
 
-        Iterator it = list1.iterator();
+         // invoke built-in element() method on the list and print the result
+         System.out.print("Element at the head of the list: " + list1.element());
+
+        Iterator <Integer> it = list1.iterator();
             while (it.hasNext()) {
                 System.out.println(it.next());
 }
-
-        // for (int i = 0; i < list1.size(); i++) {
-        //     System.out.println(list1.get(i));
-        // }
 
         return list1;
 
@@ -43,40 +47,109 @@ public class CollectionsExercises {
    
 
     public Stack<Integer> useStack() {
+
         // TODO: create an empty stack
+        Stack<Integer>myStack = new Stack<>(); 
+
         //  - add 5, 6, 8, 9 to the stack
+        myStack.push(5);
+        myStack.push(6);
+        myStack.push(8);
+        myStack.push(9);
+
         //  - print the first element of the stack on the screen
+        Integer firstIn = myStack.get(0);
+        System.out.print(firstIn);
+
         //  - print the last element of the stack on the screen
+        Integer lastIn = myStack.peek();
+        System.out.print(lastIn);
+
         //  - invoke the method pop() on the stack and print the result on the screen
+        Integer poppedOff = myStack.pop();
+        System.out.print(poppedOff);
+        
+
         //  - invoke the push(4) method on the stack
+        myStack.push(4);
+
         //  - return the stack
-        throw new RuntimeException("Not implemented");
+        return myStack;
+
     }
 
     public ArrayDeque<Integer> useArrayDeque() {
+
         // TODO: create an empty arrayDeque
+        ArrayDeque <Integer> myDeque = new ArrayDeque<>();
+
         //  - add 5, 6, 8, 9 to the queue
+        myDeque.add(5);
+        myDeque.add(6);
+        myDeque.add(8);
+        myDeque.add(9);
+
         //  - print the first element of the queue on the screen
+        Integer firstDeq =  myDeque.getFirst();
+        System.out.print(firstDeq);
+
         //  - print the last element of the queue on the screen
+
+        Integer lastDeq = myDeque.getLast();
+        System.out.print(lastDeq);
+
         //  - invoke the method poll() on the queue and print the result on the screen
+        Integer pollReturn = myDeque.poll();
+        System.out.print(pollReturn);
+
         //  - invoke the element() method on the queue and print the result on the screen
+        Integer similarGetFirst = myDeque.element();
+        System.out.print(similarGetFirst);
+
         //  - return the queue
-        throw new RuntimeException("Not implemented");
+        return myDeque;
     }
 
     public HashMap<Integer, String> useHashMap() {
+
         // TODO: create an empty hash map
+        HashMap<Integer, String> myHashie = new HashMap<>(); 
+
         //  - add {1, TypeScript} entry to the map
+        myHashie.put(1, "TypeScript");
+
         //  - add {2, Kotlin} entry to the map
+        myHashie.put(2, "Kotlin"); 
+
         //  - add {3, Python} entry to the map
+        myHashie.put(3, "Python");
+
         //  - add {4, Java} entry to the map
+        myHashie.put(4, "Java");
+
+
         //  - add {5, JavaScript} entry to the map
+        myHashie.put(5, "Javascript");
+
         //  - add {6, Rust} entry to the map
+        myHashie.put(6, "Rust"); 
+
+
         //  - determine the set of keys from the map and print it on the screen
-        //  - determine the set of values from the map and print it on the screen
+        var findKeys = myHashie.keySet(); 
+        System.out.print(findKeys);
+
+  //  - determine the set of values from the map and print it on the screen
+        var findValues = myHashie.values();
+        System.out.print(findValues); 
+
+      
         //  - determine whether the map contains "English" as a language and print the result on the screen
+        var englishCheck = myHashie.containsValue("English"); 
+        System.out.print(englishCheck);
+
         //  - return the map
-        throw new RuntimeException("Not implemented");
+      return myHashie;
     }
 
     public String getName() {
